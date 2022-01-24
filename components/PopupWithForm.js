ca7b58bdc2676c65a50ec0afc12aa4788b90d1be
Smauga <91209285+Smauga@users.noreply.github.com>
2022-01-24 22:36:04 +0700

    dev: редактирование popupWithForm

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,7 +1,40 @@
 import Popup from "./Popup";
 
 export default class PopupWithForm extends Popup {
-  constructor() {
-    
+  constructor(popupSelector, submitForm) {
+    super(popupSelector);
+    this._submit = submitForm;
+  }
+
+  _getInputValues() {
+    this._inputList = this._popup.querySelectorAll('.form__input');
+    this._formValues = {};
+    this._inputList.forEach(input => {
+      this._formValues[input.name] = input.value;
+   });
+
+  return this._formValues;
+  }
+
+  _handleSubmitForm() {
+    this._submit();
+  }
+
+  setEventListeners() {
+    this._popup.addEventLisenter('submit', this._handleSubmitForm);
+    super.setEventListeners();
+  }
+
+  removeEventListeners() {
+    this._popup.removeEventLisenter('submit', this._handleSubmitForm);
+    super.removeEventListeners();
+  }
+
+  open() {
+
+  }
+
+  close() {
+
   }
 }
