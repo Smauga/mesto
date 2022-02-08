805c8091999ec559b6c99613b587b8d66355e38d
Smauga <91209285+Smauga@users.noreply.github.com>
2022-02-08 16:48:25 +0700

    dev: подключить данные пользователя к серверу

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,12 @@ import { formValidatorData } from "../utils/formValidatorData.js";
 import {
   editButton,
   addButton,
+  avatarButton,
   nameSelector,
   jobSelector,
+  avatarSelector,
   popupEditForm,
+  popupEditAvatarForm,
   nameInput,
   jobInput,
   popupAddForm
@@ -47,20 +50,40 @@ function createCard(item) {
 const popupOpenCard = new PopupWithImage('.popup_type_open-element');
 popupOpenCard.setEventListeners();
 
-// Создание и рендер секции с карточками
-const cardsList = new Section({
-  items: initialCards,
-  renderer: (item) => {
-    const newCard = createCard(item);
-    cardsList.setItem(newCard, 'append');
-    },
-  },
-  '.elements__items'
-);
-cardsList.renderItems();
+// Загрузка данных юзера с сервера
+fetch('https://nomoreparties.co/v1/cohort-35/users/me', {
+  headers: {
+    authorization: '96c866eb-92a9-4140-92ff-b1afa5e1671b'
+  }
+})
+  .then(res => res.json())
+  .then((result) => {
+    userInfo.setUserInfo(result);
+    userInfo.setUserAvatar(result);
+  }); 
+
+// Создание и рендер секции карточек с сервера
+fetch('https://mesto.nomoreparties.co/v1/cohort-35/cards', {
+  headers: {
+    authorization: '96c866eb-92a9-4140-92ff-b1afa5e1671b'
+  }
+})
+  .then(res => res.json())
+  .then((result) => {
+    const cardsList = new Section({
+      items: result,
+      renderer: (item) => {
+        const newCard = createCard(item);
+        cardsList.setItem(newCard, 'append');
+        },
+      },
+      '.elements__items'
+    );
+    cardsList.renderItems();
+  }); 
 
 // Создание класса данных пользователя и получение данных
-const userInfo = new UserInfo({nameSelector: nameSelector, jobSelector: jobSelector});
+const userInfo = new UserInfo({nameSelector: nameSelector, jobSelector: jobSelector, avatarSelector: avatarSelector});
 
 // Создание попапа добавления карточки и установка слушателей
 const popupAddCard = new PopupWithForm('.popup_type_add-element', (inputValues) => {
@@ -69,16 +92,43 @@ const popupAddCard = new PopupWithForm('.popup_type_add-element', (inputValues) 
 });
 popupAddCard.setEventListeners();
 
+// Создание попапа изменения аватара и установка слушателей
+const popupEditAvatar = new PopupWithForm('.popup_type_edit-avatar', (inputValues) => {
+  userInfo.setUserAvatar(inputValues);
+  fetch('https://mesto.nomoreparties.co/v1/cohort-35/users/me/avatar ', {
+    method: 'PATCH',
+    headers: {
+      authorization: '96c866eb-92a9-4140-92ff-b1afa5e1671b',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      avatar: userInfo.getUserAvatar()
+    })
+  });
+});
+popupEditAvatar.setEventListeners();
+
 // Создание попапа редактирования профиля и установка слушателей
 const popupEditProfile = new PopupWithForm('.popup_type_edit-profile', (inputValues) => {
   userInfo.setUserInfo(inputValues);
+  fetch('https://mesto.nomoreparties.co/v1/cohort-35/users/me', {
+    method: 'PATCH',
+    headers: {
+      authorization: '96c866eb-92a9-4140-92ff-b1afa5e1671b',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: userInfo.getUserInfo().name,
+      about: userInfo.getUserInfo().job
+    })
+  });
 });
 popupEditProfile.setEventListeners();
 
 // Добавление слушателя на кнопку добавления карточки
 addButton.addEventListener('click', () => {
   formValidators[popupAddForm.getAttribute('name')].resetValidation();
-  popupAddCard.open()
+  popupAddCard.open();
 });
 
 // Добавление слушателя на кнопку редактирования профиля
@@ -89,3 +139,14 @@ editButton.addEventListener('click', () => {
   formValidators[popupEditForm.getAttribute('name')].resetValidation();
   popupEditProfile.open();
 });
+ 
+// Добавление слушателя на кнопку изменения аватара
+avatarButton.addEventListener('click', () => {
+  formValidators[popupEditAvatarForm.getAttribute('name')].resetValidation();
+  popupEditAvatar.open();
+});
+
+
+  
+// Токен: 96c866eb-92a9-4140-92ff-b1afa5e1671b
+// Идентификатор группы: cohort-35
